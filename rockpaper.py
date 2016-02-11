e96607ae7750dbc31af6e1801348fc2fbba24f90
David Bouchare <dialindo7@gmail.com>
2016-02-11 11:08:46 +0100

    Fixed indentation and improved error handling

--- rockpaper.py
+++ rockpaper.py
@@ -1,5 +1,6 @@
 import random
 import time
+import sys
 
 print("****************************************")
 print("Welcome to the rock-paper-scissors game!")
@@ -10,40 +11,45 @@ options = {1: 'Paper', 2: 'Rock', 3:'Scissors'}
 
 #Store the player's choice
 def players_choice():
-	#Get the players choice
-	while True:
-		pchoice = int(input("Enter your choice:\n 1 - Paper\n 2 - Rock\n 3 - Scissors\n"))
-		if (pchoice >= 1 and pchoice <= 3):
-			break
-		print("you chose", options[pchoice])
-	return pchoice
+  #Get the players choice
+  while True:
+  	pchoice = input("Enter your choice:\n 1 - Paper\n 2 - Rock\n 3 - Scissors\n or Q to exit\n")
+  	if pchoice == 'Q' or pchoice == 'q':
+  	  sys.exit("Goodbye!")
+  	elif (pchoice == '1' or pchoice == '2' or pchoice == '3'):
+  	  break
+  	else:
+  	  print("Not a valid choice, try again!")
+  print("You chose", options[int(pchoice)])
+  pchoice = int(pchoice)
+  return pchoice
 
 def computers_choice():
-	#Computer chooses 
-	comp_choice = random.randrange(1, 3)
-	#Displays the computer's choice
-	print("computer chose", options[comp_choice])
-	return comp_choice
+  #Computer chooses 
+  comp_choice = random.randrange(1, 3)
+  #Displays the computer's choice
+  print("computer chose", options[comp_choice])
+  return comp_choice
 
 def compare(pchoice, comp_choice):
-	result = (pchoice, comp_choice)
-	#comparisons
-	if result == (1, 1) or result == (2, 2) or result == (3, 3):
-		print("it's a tie!")
-	elif result == (1, 2) or result == (2, 3) or result == (3, 1):
-		print("you win")
-	elif result == (1, 3) or result == (2, 1) or result == (3, 2):
-		print("you lose!")
-	else:
-		print("Something went wrong")
+  result = (pchoice, comp_choice)
+  #comparisons
+  if result == (1, 1) or result == (2, 2) or result == (3, 3):
+    print("It's a tie!")
+  elif result == (1, 2) or result == (2, 3) or result == (3, 1):
+  	print("You win")
+  elif result == (1, 3) or result == (2, 1) or result == (3, 2):
+  	print("You lose!")
+  else:
+    print("Something went wrong")
 
 def turns():
-	for counter in range(1, 4):
-		print("this is round", counter)
-		pchoice = players_choice()
-		comp_choice = computers_choice()
-		compare(pchoice, comp_choice)
-	exit()
+  for counter in range(1, 4):
+  	print("This is round", counter)
+  	pchoice = players_choice()
+  	comp_choice = computers_choice()
+  	compare(pchoice, comp_choice)
+  exit()
 
 if __name__ == '__main__':
-	turns()
+  turns()
